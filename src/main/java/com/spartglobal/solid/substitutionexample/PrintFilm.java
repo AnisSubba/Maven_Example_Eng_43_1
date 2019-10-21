6d9da160c39464cbe3fee2198ee4fc537ccc7438
Anis Subba <21302329@student.uwl.ac.uk>
2019-10-21 15:52:29 +0100

    print film which extends getter/setter from Film

--- src/main/java/com/spartglobal/solid/substitutionexample/PrintFilm.java
+++ src/main/java/com/spartglobal/solid/substitutionexample/PrintFilm.java
@@ -1,4 +1,9 @@
 package com.spartglobal.solid.substitutionexample;
 
-public class PrintFilm {
+public class PrintFilm extends Film {
+
+    public void printFilm(){
+        System.out.println(getFilmName());
+    }
+
 }
